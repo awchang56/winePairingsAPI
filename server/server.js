5f832dbec2d9ba1b8a58e6bd5dbd79f3c43b80a7
Alexander Chang <awchang56@gmail.com>
2017-06-29 18:38:41 -0700

    changes port back to 3000

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
 
-app.listen(3001, () => {
-  console.log('Server connection established. Listening on port 3001');
+app.listen(3000, () => {
+  console.log('Server connection established. Listening on port 3000');
 });
 
 app.get('/', (req, res) => {
